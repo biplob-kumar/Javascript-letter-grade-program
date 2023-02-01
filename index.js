3da91b4c9d37ac20b1fd98187eac8cceb2654cd0
biplob kumar <biplobkumar125@gmail.com>
2023-02-02 00:31:13 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+//  ;atter grade program 
+
+var marks=prompt("Enter your number");
+if (marks>100 || marks<0) {
+    console.log("invalid");
+}
+else if (marks>=80 && marks<=100) {
+    console.log("A+");
+}
+else if (marks>=70 && marks<=79) {
+    console.log("A");
+}
+else if (marks>=60 && marks<=69) {
+    console.log("A-");
+}
+else if (marks>=50 && marks<=59) {
+    console.log("B");
+}
+else if (marks>=40&& marks<=49) {
+    console.log("c");
+}
+else if (marks >=33 && marks<=39) {
+    console.log("D");
+}
+else  {
+    console.log("f");
+}
+
+//  ;ater gradeprogram end 
